1694998ace667bb1cdb4eaae222c5eea4649684b
Batmagnai Monkhnasan <magnai103019@gmail.com>
2023-04-01 01:29:38 +0800

    update

--- src/components/items/__tests__/Box.test.tsx
+++ src/components/items/__tests__/Box.test.tsx
@@ -1,14 +1,6 @@
+import { customBoxData } from "@constants";
 import { render, screen } from "@testing-library/react";
-import { BoxData } from "@types";
 import Box from "../Box";
-
-const customBoxData: BoxData = {
-  color: "#FFFFFF",
-  velocity: 1.2,
-  size: "100px",
-  translateX: 0,
-  imageUrl: "https://api.dicebear.com/6.x/lorelei/svg",
-};
 
 test("It renders Box", () => {
   render(<Box scrollY={10} data={customBoxData} />);
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
+import { BoxData } from "@types";
+
 export const MIN_VELOCITY = 0.1;
 export const MAX_VELOCITY = 3;
 
@@ -6,3 +8,11 @@ export const MAX_SIZE = 300;
 
 export const REPOSITORY_URL =
   "https://github.com/Magnai1030/parallax_home_work";
+
+export const customBoxData: BoxData = {
+  color: "#FFFFFF",
+  velocity: 1.2,
+  size: "100px",
+  translateX: 0,
+  imageUrl: "https://api.dicebear.com/6.x/lorelei/svg",
+};
